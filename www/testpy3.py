335479b0f85993bfe813673a31de80cb23a3e207
Gigamish <haima1110@gmail.com>
2017-01-10 12:23:59 +0800

    1

--- www/testpy3.py
+++ www/testpy3.py
@@ -5,14 +5,14 @@ from models import User,Blog,Comment
 @asyncio.coroutine
 def test( loop ):
     yield from orm.create_pool( loop = loop, user='root', password='password', db='webapp' )
-    u=User(name='test22',email='test22@test.com',passwd='test',image='about:blank')
+    u=User(name='test23',email='test23@test.com',passwd='test',image='about:blank')
     yield from u.save()
     yield from orm.destroy_pool()
 if __name__ == '__main__':
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(test(loop))    
-   #loop.run_until_complete( asyncio.wait([test( loop )]) )
+    #loop.run_until_complete(test(loop))
+    loop.run_until_complete( asyncio.wait([test( loop )]) )
     loop.close()
     if loop.is_closed():
         sys.exit(0)
