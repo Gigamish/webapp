038072327b49c4d81ed843585d656dd06394515b
Gigamish <haima1110@gmail.com>
2017-01-10 11:32:16 +0800

    add web

--- www/testpy3.py
+++ www/testpy3.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(test(loop))    
-#loop.run_until_complete( asyncio.wait([test( loop )]) )  
+   #loop.run_until_complete( asyncio.wait([test( loop )]) )
     loop.close()
     if loop.is_closed():
         sys.exit(0)
